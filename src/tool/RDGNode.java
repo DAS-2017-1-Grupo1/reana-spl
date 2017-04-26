0e5421fcc3687bde09e5069fd2fb8bd083f012db
Lucas Couto <loc.unb@gmail.com>
2017-04-25 23:18:26 -0300

    RDGNode: classe inchada
    Operacao: extrair classe (mover metodos)
    Impactos: RDGNode.Map<RDGNode, Integer> numPathsVisit(RDGNode node, Map<RDGNode, Boolean> marks, Map<RDGNode, Map<RDGNode, Integer>> cache), RDGNode.Map<RDGNode, Integer> sumPaths(Map<RDGNode, Integer> pathsCountA, Map<RDGNode, Integer> pathsCountB), RDGNode.Map<RDGNode, Integer> getNumberOfPaths(RDGNode node), RDGNode.Map<RDGNode, Integer> getNumberMapsOfPathVisited(RDGNode node)
    Resultados:
     - Criacao de uma nova classe TopologicalSortingPaths
     - movimento dos metodos supracitados para a nova classe TopologicalSortingPaths
     - Criacao do objeto TopologicalSortingPaths na classe RDGNode

--- src/tool/RDGNode.java
+++ src/tool/RDGNode.java
@@ -1,4 +1,5 @@
 package tool;
+
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -10,23 +11,24 @@ import java.util.stream.Collectors;
 import tool.analyzers.buildingblocks.Component;
 import fdtmc.FDTMC;
 
-
 public class RDGNode {
 
-	//This reference is used to store all the RDGnodes created during the evaluation
+	// This reference is used to store all the RDGnodes created during the
+	// evaluation
 	private static Map<String, RDGNode> rdgNodes = new HashMap<String, RDGNode>();
 	private static List<RDGNode> nodesInCreationOrder = new LinkedList<RDGNode>();
 	private TopologicalSorting topoSort = new TopologicalSorting();
+	private TopologicalSortingPaths topoSortPaths = new TopologicalSortingPaths();
 
-    private static int lastNodeIndex = 0;
+	private static int lastNodeIndex = 0;
 
 	// Node identifier
 	private String id;
-	//This attribute is used to store the FDTMC for the RDG node.
+	// This attribute is used to store the FDTMC for the RDG node.
 	private FDTMC fdtmc;
 	/**
-	 * The node must have an associated presence condition, which is
-	 * a boolean expression over features.
+	 * The node must have an associated presence condition, which is a boolean
+	 * expression over features.
 	 */
 	private String presenceCondition;
 	// Nodes on which this one depends
@@ -36,21 +38,23 @@ public class RDGNode {
 	 */
 	private int height;
 
-
 	/**
-	 * The id, presence condition and model (FDTMC) of an RDG node must
-	 * be immutable, so there must be no setters for them. Hence, they
-	 * must be set at construction-time.
+	 * The id, presence condition and model (FDTMC) of an RDG node must be
+	 * immutable, so there must be no setters for them. Hence, they must be set
+	 * at construction-time.
 	 *
-	 * @param id Node's identifier. It is preferably a valid Java identifier.
-	 * @param presenceCondition Boolean expression over features (using Java operators).
-	 * @param fdtmc Stochastic model of the piece of behavioral model represented by
-	 *             this node.
+	 * @param id
+	 *            Node's identifier. It is preferably a valid Java identifier.
+	 * @param presenceCondition
+	 *            Boolean expression over features (using Java operators).
+	 * @param fdtmc
+	 *            Stochastic model of the piece of behavioral model represented
+	 *            by this node.
 	 */
 	public RDGNode(String id, String presenceCondition, FDTMC fdtmc) {
-	    this.id = id;
-	    this.presenceCondition = presenceCondition;
-	    this.fdtmc = fdtmc;
+		this.id = id;
+		this.presenceCondition = presenceCondition;
+		this.fdtmc = fdtmc;
 		this.dependencies = new HashSet<RDGNode>();
 		this.height = 0;
 
@@ -58,214 +62,142 @@ public class RDGNode {
 		nodesInCreationOrder.add(this);
 	}
 
-    public FDTMC getFDTMC() {
-        return this.fdtmc;
-    }
+	public FDTMC getFDTMC() {
+		return this.fdtmc;
+	}
 
-    public void addDependency(RDGNode child) {
-        this.dependencies.add(child);
-        height = Math.max(height, child.height + 1);
-    }
+	public void addDependency(RDGNode child) {
+		this.dependencies.add(child);
+		height = Math.max(height, child.height + 1);
+	}
 
-    public Collection<RDGNode> getDependencies() {
-        return dependencies;
-    }
+	public Collection<RDGNode> getDependencies() {
+		return dependencies;
+	}
 
-    public String getPresenceCondition() {
-        return presenceCondition;
-    }
+	public String getPresenceCondition() {
+		return presenceCondition;
+	}
 
-    public String getId() {
-        return id;
-    }
+	public String getId() {
+		return id;
+	}
 
-    /**
-     * Height of the RDGNode. This metric is defined in the same way as
-     * the height of a tree node, i.e., the maximum number of nodes in a path
-     * from this one to a leaf (node with no dependencies).
-     */
-    public int getHeight() {
-        return height;
-    }
+	/**
+	 * Height of the RDGNode. This metric is defined in the same way as the
+	 * height of a tree node, i.e., the maximum number of nodes in a path from
+	 * this one to a leaf (node with no dependencies).
+	 */
+	public int getHeight() {
+		return height;
+	}
 
-    public static RDGNode getById(String id) {
-        return rdgNodes.get(id);
-    }
+	public static RDGNode getById(String id) {
+		return rdgNodes.get(id);
+	}
 
-    public static String getNextId() {
-        return "n" + lastNodeIndex++;
-    }
+	public static String getNextId() {
+		return "n" + lastNodeIndex++;
+	}
 
-    /**
-     * We consider two RDG nodes to be equal whenever their behavior is
-     * modeled by equal FDTMCs, their presence condition is the same and
-     * their dependencies are also correspondingly equal.
-     */
-    @Override
-    public boolean equals(Object object) {
-    	boolean isEquals = false;
+	/**
+	 * We consider two RDG nodes to be equal whenever their behavior is modeled
+	 * by equal FDTMCs, their presence condition is the same and their
+	 * dependencies are also correspondingly equal.
+	 */
+	@Override
+	public boolean equals(Object object) {
+		boolean isEquals = false;
 
-    	boolean isOneRDGNode = (object != null && object instanceof RDGNode);
+		boolean isOneRDGNode = (object != null && object instanceof RDGNode);
 
-    	if (isOneRDGNode) {
-            RDGNode other = (RDGNode) object;
+		if (isOneRDGNode) {
+			RDGNode other = (RDGNode) object;
 
-            isEquals = this.getPresenceCondition().equals(other.getPresenceCondition())
-                    && this.getFDTMC().equals(other.getFDTMC())
-                    && this.getDependencies().equals(other.getDependencies());
-        }
+			isEquals = this.getPresenceCondition().equals(other.getPresenceCondition())
+					&& this.getFDTMC().equals(other.getFDTMC())
+					&& this.getDependencies().equals(other.getDependencies());
+		}
 
-    	return isEquals;
-    }
+		return isEquals;
+	}
 
-    @Override
-    public int hashCode() {
-        return id.hashCode() + presenceCondition.hashCode() + fdtmc.hashCode() + dependencies.hashCode();
-    }
+	@Override
+	public int hashCode() {
+		return id.hashCode() + presenceCondition.hashCode() + fdtmc.hashCode() + dependencies.hashCode();
+	}
 
-    @Override
-    public String toString() {
-        return getId() + " (" + getPresenceCondition() + ")";
-    }
+	@Override
+	public String toString() {
+		return getId() + " (" + getPresenceCondition() + ")";
+	}
 
-    /**
-     * Retrieves the transitive closure of the RDGNode dependency relation.
-     * The node itself is part of the returned list.
-     *
-     * It implements the Cormen et al.'s topological sort algorithm.
-     *
-     * @return The descendant RDG nodes ordered bottom-up (depended-upon to dependent).
-     * @throws CyclicRdgException if there is a path with a cycle starting from this node.
-     */
-    public List<RDGNode> getDependenciesTransitiveClosure() throws CyclicRdgException {
-        List<RDGNode> transitiveDependencies = new LinkedList<RDGNode>();
+	/**
+	 * Retrieves the transitive closure of the RDGNode dependency relation. The
+	 * node itself is part of the returned list.
+	 *
+	 * It implements the Cormen et al.'s topological sort algorithm.
+	 *
+	 * @return The descendant RDG nodes ordered bottom-up (depended-upon to
+	 *         dependent).
+	 * @throws CyclicRdgException
+	 *             if there is a path with a cycle starting from this node.
+	 */
+	public List<RDGNode> getDependenciesTransitiveClosure() throws CyclicRdgException {
+		List<RDGNode> transitiveDependencies = new LinkedList<RDGNode>();
+		topoSort.runTopologicalSortVisit(transitiveDependencies, this);
+		return transitiveDependencies;
+	}
 
-        topoSort.runTopologicalSortVisit(transitiveDependencies, this);
+	/* TopologicalSortingPaths.java */
+	public Map<RDGNode, Integer> getNumberOfPaths() throws CyclicRdgException {
+		return topoSortPaths.getNumberOfPaths(this);
+	}
 
-        return transitiveDependencies;
-    }
-
-    /*TopologicalSorting.java*/
-    /*public void runTopologicalSortVisit(List<RDGNode> transitiveDependencies, RDGNode node) {
-    	topoSort.runTopologicalSortVisit(transitiveDependencies, node);
-    }
-    
-    private boolean isRunningTopologySort(RDGNode node, Map<RDGNode, Boolean> marks) {
-    	return topoSort.isRunningTopologySort(node, marks);
-    }
-    //*********************/
-
-    // TODO Parameterize topological sort of RDG.
-    private Map<RDGNode, Integer> numPathsVisit(RDGNode node, Map<RDGNode, Boolean> marks, Map<RDGNode, Map<RDGNode, Integer>> cache) throws CyclicRdgException {
-        if (topoSort.isRunningTopologySort(node, marks)) {
-            // Mark node temporarily (cycle detection)
-            marks.put(node, false);
-
-            Map<RDGNode, Integer> numberOfPaths = new HashMap<RDGNode, Integer>();
-            // A node always has a path to itself.
-            numberOfPaths.put(node, 1);
-            // The number of paths from a node X to a node Y is equal to the
-            // sum of the numbers of paths from each of its descendants to Y.
-            for (RDGNode child: node.getDependencies()) {
-                Map<RDGNode, Integer> tmpNumberOfPaths = numPathsVisit(child, marks, cache);
-                numberOfPaths = sumPaths(numberOfPaths, tmpNumberOfPaths);
-            }
-            // Mark node permanently (finished sorting branch)
-            marks.put(node, true);
-            cache.put(node, numberOfPaths);
-            return numberOfPaths;
-        }
-        // Otherwise, the node has already been visited.
-        return cache.get(node);
-    }
-
-    /**
-     * Computes the number of paths from source nodes to every known node.
-     * @return A map associating an RDGNode to the corresponding number
-     *      of paths from a source node which lead to it.
-     * @throws CyclicRdgException
-     */
-    public Map<RDGNode, Integer> getNumberOfPaths() throws CyclicRdgException {
-        Map<RDGNode, Integer> numberOfPaths = new HashMap<RDGNode, Integer>();
-
-        Map<RDGNode, Integer> tmpNumberOfPaths = getNumberMapsOfPathVisited();
-        numberOfPaths = sumPaths(numberOfPaths, tmpNumberOfPaths);
-
-        return numberOfPaths;
-    }
-
-    private Map<RDGNode, Integer> getNumberMapsOfPathVisited() {
-        Map<RDGNode, Boolean> marks = new HashMap<RDGNode, Boolean>();
-        Map<RDGNode, Map<RDGNode, Integer>> cache = new HashMap<RDGNode, Map<RDGNode,Integer>>();
-
-        return numPathsVisit(this, marks, cache);
-    }
-
-    /**
-     * Sums two paths-counting maps
-     * @param pathsCountA
-     * @param pathsCountB
-     * @return
-     */
-    private static Map<RDGNode, Integer> sumPaths(Map<RDGNode, Integer> pathsCountA, Map<RDGNode, Integer> pathsCountB) {
-        Map<RDGNode, Integer> numberOfPaths = new HashMap<RDGNode, Integer>(pathsCountA);
-        for (Map.Entry<RDGNode, Integer> entry: pathsCountB.entrySet()) {
-            RDGNode node = entry.getKey();
-            Integer count = entry.getValue();
-            if (numberOfPaths.containsKey(node)) {
-                count += numberOfPaths.get(node);
-            }
-            numberOfPaths.put(node, count);
-        }
-        return numberOfPaths;
-    }
-
-    /**
-     * Return a {@link #equals(Object)} node
-     *
-     * @param candidate comparation element
-     * @param target submited element
-     * @return true if some candidate is equals to {@link equals method}
-     */
-    private static boolean isEqualsButNotSame(RDGNode candidate, RDGNode target) {
+	/**
+	 * Return a {@link #equals(Object)} node
+	 *
+	 * @param candidate
+	 *            comparation element
+	 * @param target
+	 *            submited element
+	 * @return true if some candidate is equals to {@link equals method}
+	 */
+	private static boolean isEqualsButNotSame(RDGNode candidate, RDGNode target) {
 		return (candidate != target && candidate.equals(target));
-    }
+	}
 
-    /**
-     * Returns the first RDG node (in crescent order of creation time) which is similar
-     * to the one provided.
-     *
-     * A similar RDG node is one for which equals() returns true.
-     * @param rdgNode
-     * @return a similar RDG node or null in case there is none.
-     */
-    public static RDGNode getSimilarNode(RDGNode target) {
-        for (RDGNode candidate: nodesInCreationOrder) {
-            if (isEqualsButNotSame(candidate, target)) {
-                return candidate;
-            }
-        }
-        return null;
-    }
+	/**
+	 * Returns the first RDG node (in crescent order of creation time) which is
+	 * similar to the one provided.
+	 *
+	 * A similar RDG node is one for which equals() returns true.
+	 * 
+	 * @param rdgNode
+	 * @return a similar RDG node or null in case there is none.
+	 */
+	public static RDGNode getSimilarNode(RDGNode target) {
+		for (RDGNode candidate : nodesInCreationOrder) {
+			if (isEqualsButNotSame(candidate, target)) {
+				return candidate;
+			}
+		}
+		return null;
+	}
 
-    /**
-     * Converts this RDG node into a Component<FDTMC>.
-     * @return
-     */
-    public Component<FDTMC> toComponent() {
-        Collection<Component<FDTMC>> dependencies = this.getDependencies().stream()
-                .map(RDGNode::toComponent)
-                .collect(Collectors.toSet());
-        return new Component<FDTMC>(this.getId(),
-                                    this.getPresenceCondition(),
-                                    this.getFDTMC(),
-                                    dependencies);
-    }
+	/**
+	 * Converts this RDG node into a Component<FDTMC>.
+	 * 
+	 * @return
+	 */
+	public Component<FDTMC> toComponent() {
+		Collection<Component<FDTMC>> dependencies = this.getDependencies().stream().map(RDGNode::toComponent)
+				.collect(Collectors.toSet());
+		return new Component<FDTMC>(this.getId(), this.getPresenceCondition(), this.getFDTMC(), dependencies);
+	}
 
-    public static List<Component<FDTMC>> toComponentList(List<RDGNode> nodes) {
-        return nodes.stream()
-                .map(RDGNode::toComponent)
-                .collect(Collectors.toList());
-    }
+	public static List<Component<FDTMC>> toComponentList(List<RDGNode> nodes) {
+		return nodes.stream().map(RDGNode::toComponent).collect(Collectors.toList());
+	}
 
 }
--- src/tool/TopologicalSorting.java
+++ src/tool/TopologicalSorting.java
@@ -5,64 +5,64 @@ import java.util.List;
 import java.util.Map;
 
 public class TopologicalSorting {
-	
-	
-	
+
 	/**
-     * Run {@link #topoSortVisit(RDGNode, Map, List)} to get one sort graph
-     *
-     * @param transitiveDependencies
-     */
-	
+	 * Run {@link #topoSortVisit(RDGNode, Map, List)} to get one sort graph
+	 *
+	 * @param transitiveDependencies
+	 */
+
 	public void runTopologicalSortVisit(List<RDGNode> transitiveDependencies, RDGNode node) {
-    	Map<RDGNode, Boolean> marks = new HashMap<RDGNode, Boolean>();
+		Map<RDGNode, Boolean> marks = new HashMap<RDGNode, Boolean>();
 
-    	topoSortVisit(node, marks, transitiveDependencies);
-    }
-	
+		topoSortVisit(node, marks, transitiveDependencies);
+	}
+
 	/**
-     * Topological sort {@code visit} function (Cormen et al.'s algorithm).
-     * @param node
-     * @param marks
-     * @param sorted
-     * @throws CyclicRdgException
-     */
-	public void topoSortVisit(RDGNode node, Map<RDGNode, Boolean> marks, List<RDGNode> sorted) throws CyclicRdgException {
-        if (isRunningTopologySort(node, marks)) {
-            // Mark node temporarily (cycle detection)
-            marks.put(node, false);
+	 * Topological sort {@code visit} function (Cormen et al.'s algorithm).
+	 * 
+	 * @param node
+	 * @param marks
+	 * @param sorted
+	 * @throws CyclicRdgException
+	 */
+	private void topoSortVisit(RDGNode node, Map<RDGNode, Boolean> marks, List<RDGNode> sorted)
+			throws CyclicRdgException {
+		if (isRunningTopologySort(node, marks)) {
+			// Mark node temporarily (cycle detection)
+			marks.put(node, false);
 
-            for (RDGNode child: node.getDependencies()) {
-                topoSortVisit(child, marks, sorted);
-            }
+			for (RDGNode child : node.getDependencies()) {
+				topoSortVisit(child, marks, sorted);
+			}
 
-            // Mark node permanently (finished sorting branch)
-            marks.put(node, true);
-            sorted.add(node);
-        }
-    }
-	
+			// Mark node permanently (finished sorting branch)
+			marks.put(node, true);
+			sorted.add(node);
+		}
+	}
+
 	/**
-     * Auxiliar method to topological sort. See {@link #topoSortVisit(RDGNode, Map, List)}
-     *
-     */
+	 * Auxiliar method to topological sort. See
+	 * {@link #topoSortVisit(RDGNode, Map, List)}
+	 *
+	 */
 	public boolean isRunningTopologySort(RDGNode node, Map<RDGNode, Boolean> marks) {
-    	boolean isRunning = false;
+		boolean isRunning = false;
 
-    	if (hasAtLeastOneCycle(marks, node)) {
-	        // Visiting temporarily marked node -- this means a cyclic dependency!
-	        throw new CyclicRdgException();
-	    } else if (!marks.containsKey(node)) {
-	    	isRunning = true;
-	    }
+		if (hasAtLeastOneCycle(marks, node)) {
+			// Visiting temporarily marked node -- this means a cyclic
+			// dependency!
+			throw new CyclicRdgException();
+		} else if (!marks.containsKey(node)) {
+			isRunning = true;
+		}
 
-	    return isRunning;
-    }
-	
-	public boolean hasAtLeastOneCycle(Map<RDGNode, Boolean> marks, RDGNode node) {
-    	return marks.containsKey(node) && marks.get(node) == false;
+		return isRunning;
+	}
+
+	private boolean hasAtLeastOneCycle(Map<RDGNode, Boolean> marks, RDGNode node) {
+		return marks.containsKey(node) && marks.get(node) == false;
 	}
 
 }
-
-
